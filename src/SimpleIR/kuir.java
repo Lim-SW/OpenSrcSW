e31e6d2379ca77ae7dbb9d0d2004fe0ca49f3aa6
LimSW <dlj0605@naver.com>
2021-03-24 15:55:11 +0900

    2021-03-24 -c, -k 인자값 처리 최종

--- src/SimpleIR/kuir.java
+++ src/SimpleIR/kuir.java
@@ -10,20 +10,32 @@ public class kuir {
 		makeCollection mc = new makeCollection();
 		makeKeyword mk = new makeKeyword();
 		//String PATH = "./html";
-		String PATH = args[0];
 		
-		if(PATH.contains(".xml")) { // xml 파일로 인자값 줄시
+		String COMP = args[0]; // -c -k -i
+		String PATH = args[1]; // 폴더경로나 xml파일
+		
+		//System.out.println(COMP);
+		//System.out.println(PATH);
+		
+		if(COMP.equals("-c")) { // 0번 인자값이 -c
+			Document doc = mc.setXML(); // doc 초기파일 작성
+			doc = mc.makeBody(doc, PATH); // body에 내용 채우기
+			mc.makeXML("collection", doc); // XML파일 생성 (만들파일이름, doc)
+			System.out.println("collection.xml 생성 완료");
+		}
+		
+		else if(COMP.equals("-k")) { // 0번 인자값이 -k
 			Document doc = mk.getDoc(PATH);
 			doc = mk.kkma(doc); // 형태소 작업
 			mc.makeXML("index",doc); // XML파일 생성 (형태소 작업 한거)
 			System.out.println("index.xml 생성 완료");
 		}
 		
-		else { // html 디렉토리 // html 디렉토리로 인자값 줄시
-			Document doc = mc.setXML(); // doc 초기파일 작성
-			doc = mc.makeBody(doc, PATH); // body에 내용 채우기
-			mc.makeXML("collection", doc); // XML파일 생성 (만들파일이름, doc)
-			System.out.println("collection.xml 생성 완료");
+		//-i 추가 해야함 2021.03.25 실습날
+		
+		else { // 0번 인자값이 엉뚱한값
+			System.out.println("args[0]에 올바른 인자값을 넣어주세요.");
+			System.out.println("ex) -c , -k, -i");
 		}
 		
 	}
