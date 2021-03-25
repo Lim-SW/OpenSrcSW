eca76c579f067602c66fe6d405eb66df8b14d369
LimSW <dlj0605@naver.com>
2021-03-25 19:15:41 +0900

    value값 float 리스트로변경 / 자연로그로 다시계산&소수점 셋째자리에서반올림 / 문서에없는 키워드 0.0줬던거 다시 뺌

--- src/SimpleIR/indexer.java
+++ src/SimpleIR/indexer.java
@@ -53,7 +53,7 @@ public class indexer {
 		//List TextList = new ArrayList();
 		
 		String Sample = "";
-		
+		/*
 		//틀 만들기
 		for(int l=0;l<N;l++) {
 			if(l==0) {
@@ -76,6 +76,7 @@ public class indexer {
 				}
 			}
 		}
+		*/ // 0 안넣어도 되어서 필요 없어짐 ㅠ.ㅠ
 		/*
 		for(int i=0;i<Wlist.size()-5;i+=6) { //리스트에 잘 저장되어있는지 확인
 			System.out.println(Wlist.get(i));
@@ -83,6 +84,7 @@ public class indexer {
 			System.out.println();
 		}
 		*/
+		
 		for(int i=0;i<N/*DocArr.size()*/;i++) {
 			
 			String tempDoc = DocArr.get(i).toString();
@@ -114,7 +116,7 @@ public class indexer {
 					System.out.println(df);
 				}
 				*/
-				w = tf*Math.log10(N/df); // w 값 계산
+				w = tf*Math.log(N/df); // w 값 계산
 				//System.out.print(strText);
 				//System.out.print(" N:"+N); // 값 확인용
 				//System.out.print(" tf:"+tf);
@@ -130,19 +132,22 @@ public class indexer {
 				*/
 				df=0; // df값 초기화
 				
-				//여기서 w 해줘야함
+				//여기서 w 해줘야함 (필요없어짐...) 밑에서 함
+				/*
 				int Wtemp = Wlist.indexOf(strText)+1; // 단어의 가중치모음 인덱스
 				//System.out.println(Wtemp);
 				String StrT1 = Wlist.get(Wtemp).toString(); //가중치 모음 가져오기
 				//System.out.println(StrT1);
 				String[] split = null;
 				split = StrT1.split(" "); // 가중치 쪼개기 (docid / 가중치 / docid / 가중치 / ...)
+				*/
 				/*
 				for(int o=0;o<split.length;o++) {
 					System.out.println(split[o]);
 				}
 				*/
 				//System.out.println(split.length);
+				/*
 				split[(i*2)+1] = Double.toString(w); // 계산된 적절한 위치에 가중치 덮어쓰기
 				String StrT2="";
 				for(int s=0;s<split.length;s++) { // 쪼갠거 다시 합하기
@@ -156,18 +161,18 @@ public class indexer {
 				//System.out.println(StrT2);
 				
 				Wlist.set(Wtemp, StrT2); // 수정된 가중치 적용
+				*/ // 여기도 필요없어짐 ㅠㅠ
 				
-				/*
 				if(Wlist.contains(strText)) { // 문서의 단어가 리스트에 이미 존재하면 새로만들지 않고 기존에 추가
 					int Wtemp = Wlist.indexOf(strText)+1; // 짝수=key 홀수=tf-idf 모음
 					//System.out.println("홀수:"+Wtemp); // 이거 무조건 홀수여야함(test)
-					Wlist.set(Wtemp, w);	
+					Wlist.set(Wtemp, Wlist.get(Wtemp)+" "+i+" "+w);	//옆에 추가하기
 				}
 				else { // 문서의 단어가 처음등장 한거라면 리스트에 새로 추가
-					//Wlist.add(strText);
-					//Wlist.add(i+" "+w);
+					Wlist.add(strText);
+					Wlist.add(i+" "+w);
 				}
-				*/
+				
 			}
 		}
 		
@@ -212,6 +217,8 @@ public class indexer {
 		*/
 		List Wlist = calculateWeight(DocArr, BigArr); // tf-idf 계산 작업
 		
+		changeFloat(Wlist); // float로 바꿈
+		
 		//hm.put(word, weight);
 		
 		//여기서 반복문으로 한번에 만들어야함
@@ -221,11 +228,12 @@ public class indexer {
 		/*
 		for(int i=0;i<Wlist.size()-1;i+=2) { //리스트에 잘 저장되어있는지 확인
 			System.out.println(Wlist.get(i));
-			System.out.println(Wlist.get(i+1).toString());
+			System.out.println(Wlist.get(i+1));
 			//System.out.println((i+1)+"/"+(Wlist.size()-1));
 			System.out.println();
 		}
 		*/
+		
 		/*
 		for(int i=0;i<Wlist.size()-1;i+=2) { //해쉬맵에 잘 저장되어있는지 확인
 			System.out.print(Wlist.get(i)+" ");
@@ -236,4 +244,47 @@ public class indexer {
 		objectOutputStream.writeObject(hm);
 		objectOutputStream.close();
 	}
+	
+	public List changeFloat(List list) { // 리스트 받으면 가중치 떄서 float 리스트로 만들고 리턴
+		 // 이걸 붙이기용 리스트로 쓰자
+		List tempA = new ArrayList();
+		
+		for(int i=0;i<list.size()-1;i+=2) {
+			String[] split = null;
+			String temp ="";
+			List array = new ArrayList();
+			temp = list.get((i+1)).toString();
+			split = temp.split(" ");
+			for(int j=0;j<split.length;j++) {
+				float f = Float.parseFloat(split[j]);
+				//System.out.println(f);
+				//f = (float) (Math.round(f*100)/100.0); // 셋째 자리에서 반올림
+				//System.out.println(f);
+				array.add(f);
+				//System.out.println(array);
+			}
+			
+			list.set(i+1, array);
+			array = new ArrayList();
+			//array.clear();
+			/*
+			for(int l=0;l<array.size();l++) { //리스트에 잘 저장되어있는지 확인
+				System.out.println(array.get(l));
+				//System.out.println(array.get(l+1));
+				//System.out.println((i+1)+"/"+(Wlist.size()-1));
+				System.out.println();
+			}*/
+		}
+		/*
+		for(int i=0;i<list.size()-1;i+=2) { //리스트에 잘 저장되어있는지 확인
+			System.out.println(list.get(i));
+			System.out.println(list.get(i+1));
+			//System.out.println((i+1)+"/"+(Wlist.size()-1));
+			System.out.println();
+		}
+		*/
+		return list;
+	}
 }
+
+	
